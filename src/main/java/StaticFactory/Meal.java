d52057a10fb5c7c70b650e349b2566f07be1a284
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-23 23:48:07 +0300

    add meal super class

---
+++ src/main/java/StaticFactory/Meal.java
@@ -0,0 +1,13 @@
+package StaticFactory;
+
+public class Meal {
+    String name;
+
+
+    public Meal(String name) {
+        this.name = name;
+    }
+    public static IceCream coldMeal(String name) {
+        return new IceCream();
+    }
+}
