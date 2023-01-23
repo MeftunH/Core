33451b08921c105f891ecf2b0ac3d2a1de8a5931
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-23 23:48:27 +0300

    add team class

---
+++ src/main/java/StaticFactory/Team.java
@@ -0,0 +1,12 @@
+package StaticFactory;
+
+public class Team {
+    private String name;
+    private int age;
+
+    public Team(String name, int age) {
+        //Todo: Auto-generate constructor stub
+        this.name = name;
+        this.age = age;
+    }
+}
