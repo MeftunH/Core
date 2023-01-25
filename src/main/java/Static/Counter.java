309845185aba0df268fb4cf24039ddbae5cf02c8
Maftun Hashimli <maftunhashimli@gmail.com>
2023-01-25 23:46:34 +0300

    add counter class

---
+++ src/main/java/Static/Counter.java
@@ -0,0 +1,18 @@
+package Static;
+
+public class Counter {
+    static int count=0;//will get memory only once and retain its value
+
+    Counter(){
+        count++;//incrementing the value of static variable
+        System.out.println(count);
+    }
+
+    public static void main(String args[]){
+//creating objects
+        Counter c1=new Counter();
+        Counter c2=new Counter();
+        Counter c3=new Counter();
+    }
+}
+
