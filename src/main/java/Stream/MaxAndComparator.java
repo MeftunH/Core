870d26dbafd5b8c4dab985cf2c9519932403657e
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 01:39:15 +0300

    add max and comparator class

---
+++ src/main/java/Stream/MaxAndComparator.java
@@ -0,0 +1,29 @@
+package Stream;
+
+import java.util.List;
+
+public class MaxAndComparator {
+    public static Person getOldestPerson(List<Person> people) {
+        Person oldestPerson = new Person("",0,"");
+        for (Person person : people)
+        {
+            if (person.getAge() > oldestPerson.getAge())
+            {
+                oldestPerson = person;
+            }
+        }
+        return oldestPerson;
+    }
+
+    public static Person getOldestPersonStream(List<Person> people) {
+        Person oldestPerson = new Person("",0,"");
+        for (Person person : people)
+        {
+            if (person.getAge() > oldestPerson.getAge())
+            {
+                oldestPerson = person;
+            }
+        }
+        return oldestPerson;
+    }
+}
