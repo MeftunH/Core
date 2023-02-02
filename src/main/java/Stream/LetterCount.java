d3851f1616e160fcabed960021b26ac580648a0e
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-02 23:38:09 +0300

    set letter count class

---
+++ src/main/java/Stream/LetterCount.java
@@ -0,0 +1,21 @@
+package Stream;
+
+import java.util.Arrays;
+
+public class LetterCount {
+    public static void main(String[] args) {
+
+    }
+
+    //GET ARRAY'S ELEMENTS => stream()
+    //FILTER NAMES LONGER THAN 5 => filter()
+    //GET NUMBER OF THE OTHER LETTERS FROM LIST =? mapToInt
+    //SUM OF LETTERS => sum()
+    public static int getTotalNumberOfLettersOfNamesLongerThanFive(String... names) {
+        return Arrays.stream(names)
+                .filter(name -> name.length() > 5)
+                .mapToInt(name -> name.length())
+                .sum()
+                ;
+    }
+}
