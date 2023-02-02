f67710eebf7a165ca4b50f82ea24372eb46d3ddb
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 01:38:55 +0300

    addd person class

---
+++ src/main/java/Stream/Person.java
@@ -0,0 +1,23 @@
+package Stream;
+
+public class Person {
+    private String name;
+    private int age;
+    private String nationality;
+
+    public Person(String name, int age, String nationality) {
+      this(name,age,"");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+}
