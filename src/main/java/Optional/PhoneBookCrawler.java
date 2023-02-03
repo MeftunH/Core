544a1dc3edc9538e07096c669edb3e1c3965f949
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 05:44:33 +0300

    declare phone book crawler problem

---
+++ src/main/java/Optional/PhoneBookCrawler.java
@@ -0,0 +1,27 @@
+package Optional;
+
+public class PhoneBookCrawler {
+
+    private PhoneBook phoneBook;
+
+    public PhoneBookCrawler(PhoneBook phoneBook) {
+        this.phoneBook = phoneBook;
+    }
+
+    public String findPhoneNumberByNameAndPunishIfNothingFound(String name){
+        return null;
+    }
+
+    public String findPhoneNumberByNameAndPrintPhoneBookIfNothingFound(String name){
+        return null;
+    }
+
+    public String findPhoneNumberByNameOrNameByPhoneNumber(String name, String phoneNumber){
+        return null;
+    }
+
+    public PhoneBook getPhoneBook(){
+        return phoneBook;
+    }
+
+}
