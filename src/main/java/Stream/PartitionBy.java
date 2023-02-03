ed127a81a3211b3a40b9f34445298d8b35a8e666
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 03:40:50 +0300

    add partition by example

--- src/main/java/Stream/PartitionBy.java
+++ src/main/java/Stream/PartitionBy.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class PartitionBy {
     public static Map<Boolean, List<Person>> partitionAdults(List<Person> people) {
@@ -15,5 +17,13 @@ public class PartitionBy {
             map.get(person.getAge()>=18).add(person);
         }
         return map;
+    }
+
+    //List<Integer> : 1,2,3,4,5,6
+    //Group by greater or smaller than 18
+    public static Map<Boolean, List<Person>> partitionAdultsByStream(List<Person> people) {
+        return people.stream()
+                .collect(Collectors.groupingBy(person->person.getAge() >= 18, Collectors.toList()));
+        //people.stream.filter(person.getAge >= 18).collect(Collectors.toList());
     }
 }
