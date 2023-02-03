ef1753f6db29082216234719435296685973fff8
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 05:21:46 +0300

    solve add prefix to string

---
+++ src/main/java/Stream/AddPrefixToString.java
@@ -0,0 +1,29 @@
+package Stream;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AddPrefixToString {
+    public static void main(String[] args) {
+        AddPrefixToString.getString(Arrays.asList(3,44)); //isEqualTo("o3,e44")
+        AddPrefixToString.getString(Collections.singletonList(3));//isEqualTo("o3")
+    }
+    //add o to odd and e to double numbers
+    private static String getString(List<Integer> asList) {
+        return asList.stream()
+                .map(i->{
+                    String s = "";
+                    if (i%2 == 0){
+                        s+="e" + i;
+                    }
+                    else {
+                        s += "o" + i;
+                    }
+                    return s;
+                })   //Stream<String>
+                .collect(Collectors.joining(","))
+                ;
+    }
+}
