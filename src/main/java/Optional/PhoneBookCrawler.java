9796eeb320f00341e0103094cb1b3b32c5611fae
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 05:50:03 +0300

    set phone book crawler solution

--- src/main/java/Optional/PhoneBookCrawler.java
+++ src/main/java/Optional/PhoneBookCrawler.java
@@ -4,12 +4,16 @@ public class PhoneBookCrawler {
 
     private PhoneBook phoneBook;
 
+    public static void main(String[] args) {
+//        findPhoneNumberByNameAndPunishIfNothingFound("Maftun").toUpperCase();
+    }
+
     public PhoneBookCrawler(PhoneBook phoneBook) {
         this.phoneBook = phoneBook;
     }
 
     public String findPhoneNumberByNameAndPunishIfNothingFound(String name){
-        return null;
+        return phoneBook.findPhoneNumberByName(name).orElseThrow(() -> new IllegalArgumentException("No phone number found"));
     }
 
     public String findPhoneNumberByNameAndPrintPhoneBookIfNothingFound(String name){
