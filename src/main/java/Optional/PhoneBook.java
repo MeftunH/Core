c1510b2075cf69ffc2ac9cba6579288a858f51c4
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-03 05:42:15 +0300

    solve phone book optional problem

--- src/main/java/Optional/PhoneBook.java
+++ src/main/java/Optional/PhoneBook.java
@@ -1,6 +1,7 @@
 package Optional;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public class PhoneBook {
@@ -8,7 +9,7 @@ public class PhoneBook {
     private static final HashMap<String, String> PHONE_NUMBERS = new HashMap<String, String>()
     {
         {
-            put("Jos de Vos", "016/161616");
+            put("Jos de Vos", "016/161616"); //null
             put("An de Toekan", "016/161617");
             put("Kris de Vis", "016/161618");
         }
@@ -22,12 +23,21 @@ public class PhoneBook {
         return phoneBookEntries;
     }
 
+//    public Optional<String> findPhoneNumberByName(String name){
+//        return null;
+//    }
     public Optional<String> findPhoneNumberByName(String name){
-        return null;
+        return Optional.ofNullable(PHONE_NUMBERS.get(name));
     }
-
+//    public Optional<String> findNameByPhoneNumber(String phoneNumber){
+//        return null;
+//    }
     public Optional<String> findNameByPhoneNumber(String phoneNumber){
-        return null;
+        return PHONE_NUMBERS.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().equals(phoneNumber))
+                .map(Map.Entry::getKey)
+                .findFirst();
     }
 
     @Override
