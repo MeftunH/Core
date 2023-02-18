8c9dc25747334ad9ee65d1472b176650f45b480c
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-18 23:45:12 +0300

    add factorial example

---
+++ src/main/java/Recursion/factorial/Factorial.java
@@ -0,0 +1,13 @@
+package Recursion.factorial;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Factorial {
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+}
+
---
+++ src/main/java/Recursion/factorial/Main.java
@@ -0,0 +1,10 @@
+package Recursion.factorial;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Main {
+    public static void main(String[] args) {
+        int n=5;
+        int result=Factorial.factorial(n);
+        System.out.println("Factorial of "+n+" is "+result);
+    }
+}
