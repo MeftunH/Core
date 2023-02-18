75b6e81b5201b4d00d832b4223e0eb1d28bc7d08
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-18 23:45:26 +0300

    add recursion file search example

---
+++ src/main/java/Recursion/FileSearch.java
@@ -0,0 +1,24 @@
+package Recursion;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import java.io.File;
+
+public class FileSearch {
+    public static boolean searchForFile(File directory, String fileName) {
+        if (directory.isDirectory()) {
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory()) {
+                        if (searchForFile(file, fileName)) {
+                            return true;
+                        }
+                    } else if (file.getName().equals(fileName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
