529446446efee49dfc5b1ed47107fea385d5f484
Maftun Hashimli <maftunhashimli@gmail.com>
2023-02-28 23:58:02 +0300

    add drawable without lambda function

--- src/main/java/Lambda/Drawable.java
+++ src/main/java/Lambda/Drawable.java
@@ -5,7 +5,13 @@ package Lambda;
 public class Drawable {
     public static void main(String[] args) {
         int width=10;
-
+        //without lambda
+        DrawableInterface d1=new DrawableInterface() {
+            @Override
+            public void draw() {
+                System.out.println("Drawing "+width);
+            }
+        };
         //with lambda
         DrawableInterface d2=()->{
             System.out.println("Drawing "+width);
