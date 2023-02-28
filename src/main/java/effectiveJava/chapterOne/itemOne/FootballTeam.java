a2b8688bf582795bbce30539988f69dff9682c44
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-01 01:46:16 +0300

    set football team example

---
+++ src/main/java/effectiveJava/chapterOne/itemOne/FootballTeam.java
@@ -0,0 +1,16 @@
+package effectiveJava.chapterOne.itemOne;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class FootballTeam {
+    private String newFootballer;
+    private int age;
+
+    public FootballTeam(String newFootballer, int age) {
+        this.newFootballer=newFootballer;
+        this.age=age;
+    }
+    public static FootballTeam createNewFootballer(String newFootballer)
+    {
+        return new FootballTeam(newFootballer,22);
+    }
+}
--- src/main/java/effectiveJava/chapterOne/itemOne/Main.java
+++ src/main/java/effectiveJava/chapterOne/itemOne/Main.java
@@ -4,5 +4,9 @@ package effectiveJava.chapterOne.itemOne;
 public class Main {
     public static void main(String[] args) {
         Food food =  Food.hotFood("name");
+        //we want to have our all footballers age 22
+        //        FootballTeam footballTeam=new FootballTeam("Maftun",22);
+        FootballTeam footballTeam1=FootballTeam.createNewFootballer("Ronaldo");
+        FootballTeam footballTeam2=FootballTeam.createNewFootballer("Messi");
     }
 }
