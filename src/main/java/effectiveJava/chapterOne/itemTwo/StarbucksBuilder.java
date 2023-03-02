9001425be518c6b9e6ff8f2378074eb3b5b5288b
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-02 07:53:10 +0300

    set sb builder

---
+++ src/main/java/effectiveJava/chapterOne/itemTwo/StarbucksBuilder.java
@@ -0,0 +1,42 @@
+package effectiveJava.chapterOne.itemTwo;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class StarbucksBuilder {
+    //Required
+    private String size;
+
+    private String nonLactose;
+    private String sugar;
+
+    public static class Builder{
+        private final String size;
+
+        private String nonLactose;
+        private String sugar;
+
+        public Builder(String size)
+        {
+            super();
+            this.size=size;
+        }
+        public Builder nonLactose(String nonLactose)
+        {
+            this.nonLactose=nonLactose;
+            return this;
+        }
+        public Builder sugar(String sugar)
+        {
+            sugar=sugar;
+            return this;
+        }
+        public StarbucksBuilder build()
+        {
+            return new StarbucksBuilder(this);
+        }
+    }
+    public StarbucksBuilder(Builder builder) {
+        size=builder.size;
+        nonLactose=builder.nonLactose;
+        sugar=builder.nonLactose;
+    }
+}
