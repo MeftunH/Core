5132e1a155b1229afb9c7adc5edafbe35c17ad36
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-04 23:56:27 +0300

    add public example

--- src/main/java/effectiveJava/chapterFour/AccessModifier.java
+++ src/main/java/effectiveJava/chapterFour/AccessModifier.java
@@ -5,4 +5,7 @@ public class AccessModifier {
     private void message() {
         System.out.println("Hello");
     }
+    public String publicMessage() {
+        return "Hello Public";
+    }
 }
