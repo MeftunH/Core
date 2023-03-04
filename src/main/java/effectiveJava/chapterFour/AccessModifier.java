04f6867b163fb62268b7ba007c281380c917049a
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-04 23:55:30 +0300

    add private example

---
+++ src/main/java/effectiveJava/chapterFour/AccessModifier.java
@@ -0,0 +1,8 @@
+package effectiveJava.chapterFour;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class AccessModifier {
+    private void message() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/effectiveJava/chapterFour/Main.java
@@ -0,0 +1,10 @@
+package effectiveJava.chapterFour;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Main {
+    public static void main(String[] args) {
+        AccessModifier am=new AccessModifier();
+        //we can't access private method from another class
+        //        am.message();
+    }
+}
