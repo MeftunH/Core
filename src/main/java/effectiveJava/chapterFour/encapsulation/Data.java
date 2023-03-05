1967aab772c33ed8d04faf319e64e5713e151755
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-05 23:47:07 +0300

    add encapsulation example

---
+++ src/main/java/effectiveJava/chapterFour/encapsulation/Data.java
@@ -0,0 +1,32 @@
+package effectiveJava.chapterFour.encapsulation;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Data {
+    private String name;
+    private int value;
+
+    public Data(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Data{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/main/java/effectiveJava/chapterFour/encapsulation/Main.java
@@ -0,0 +1,11 @@
+package effectiveJava.chapterFour.encapsulation;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Main {
+    public static void main(String[] args) {
+        Data data=new Data("Maftun", 25);
+        System.out.println(data);
+        data.setValue(26);
+        System.out.println(data);
+    }
+}
