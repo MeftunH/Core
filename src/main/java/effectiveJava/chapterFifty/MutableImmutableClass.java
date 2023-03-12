e0d4298880d17c2acc4df0fca5f87235bbd736c3
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-12 17:50:43 +0300

    add mutable immutable example

---
+++ src/main/java/effectiveJava/chapterFifty/MutableImmutableClass.java
@@ -0,0 +1,19 @@
+package effectiveJava.chapterFifty;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import java.util.Date;
+
+public class MutableImmutableClass {
+    public static void main(String[] args) {
+        //Immutable
+        String name="Meftun";
+        System.out.println(name.toUpperCase());
+        System.out.println(name);
+
+        //Mutable
+        Date date=new Date();
+        System.out.println(date);
+        date.setYear(2025);
+        System.out.println(date);
+    }
+}
