0d7b5bf72e7eeeca254a5c767b7327fdb7a9106d
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-13 11:37:17 +0300

    add collection classifier

---
+++ src/main/java/effectiveJava/chapterFiftyOne/CollectionClassifier.java
@@ -0,0 +1,26 @@
+package effectiveJava.chapterFiftyOne;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class CollectionClassifier {
+    public static String classify(Set<?> s) {
+        return "Set";
+    }
+    public static String classify(List<?> lst) {
+        return "List";
+    }
+    public static String classify(Collection<?> c) {
+        return "Unknown Collection";
+    }
+    public static void main(String[] args) {
+        Collection<?>[] collections = {
+                new HashSet<>(),
+                new ArrayList<>(),
+                new HashMap<String, String>().values()
+        };
+        for (Collection<?> c : collections)
+            System.out.println(classify(c));
+    }
+}
