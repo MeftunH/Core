de684249c49d448587ddc1c7c14f0cdd123ed8c5
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-13 11:39:47 +0300

    add varargs example

---
+++ src/main/java/effectiveJava/chapterFiftyThree/Varargs.java
@@ -0,0 +1,16 @@
+package effectiveJava.chapterFiftyThree;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Varargs {
+    public static void main(String[] args) {
+        System.out.println(sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    }
+
+    public static int sum(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+}
