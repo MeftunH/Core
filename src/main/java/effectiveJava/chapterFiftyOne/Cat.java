1b225ce998b918b17d7121dd549f86424c5e8de0
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-13 11:36:54 +0300

    add overriding example

---
+++ src/main/java/effectiveJava/chapterFiftyOne/Cat.java
@@ -0,0 +1,10 @@
+package effectiveJava.chapterFiftyOne;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Cat extends Animal {
+    //Overriding
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
---
+++ src/main/java/effectiveJava/chapterFiftyOne/Dog.java
@@ -0,0 +1,10 @@
+package effectiveJava.chapterFiftyOne;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Dog extends Animal {
+    //Overriding
+    @Override
+    public void makeSound() {
+        System.out.println("Woof");
+    }
+}
