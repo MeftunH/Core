c1fe065a997ab218ac0fc15b79671f44220b1322
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-14 21:21:40 +0300

    add need of collection

---
+++ src/main/java/effectiveJava/chapterFiftyFour/NeedOfCollection.java
@@ -0,0 +1,36 @@
+package effectiveJava.chapterFiftyFour;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class NeedOfCollection {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=30;
+        int d=40;
+        int e=30;
+        int f=30;
+        int g=30;
+        int h=30;
+
+        int[] arr=new int[10000];
+
+        //limitation of array
+        //1. ARRAYS ARE FIXED IN SIZE
+        //2. ARRAYS ARE NOT TYPE SAFE
+        //3. ARRAYS CAN HOLD ONLY HOMEGENEOUS DATA ELEMENTS
+        //4. ARRAYS ARE NOT GENERIC
+        Student[] students=new Student[10];
+        students[0]=new Student();
+        students[1]=new Student();
+        //compile time error
+//        students[2] = new Book();
+    }
+}
+
+class Book {
+
+}
+
+class Student {
+
+}
