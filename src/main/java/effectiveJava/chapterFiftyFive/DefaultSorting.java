f923d25fb8b3d5ae2cfe54c4265d593086c74868
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-16 23:53:52 +0300

    add default sort example

---
+++ src/main/java/effectiveJava/chapterFiftyFive/DefaultSorting.java
@@ -0,0 +1,18 @@
+package effectiveJava.chapterFiftyFive;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import java.util.Arrays;
+
+public class DefaultSorting {
+    public static void main(String[] args) {
+        String[] names = { "Alice", "Bob", "Charlie", "Dave", "Eve" };
+
+// Sort the names array in ascending order
+        Arrays.sort(names);
+
+// Print the sorted names
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
