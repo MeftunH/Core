17395d816cadd317727074ff1d76f85a05bb6b20
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-20 06:56:30 +0300

    add sync application example

---
+++ src/main/java/examples/thread/SyncApplication.java
@@ -0,0 +1,37 @@
+package examples.thread;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class SyncApplication {
+
+    private int count = 0;
+
+    public static void main(String[] args) {
+        SyncApplication application = new SyncApplication();
+        application.doCount();
+    }
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    private void doCount() {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    increment();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10000; i++) {
+                    increment();
+                }
+            }
+        });
+    }
+
+}
