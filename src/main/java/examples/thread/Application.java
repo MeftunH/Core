c6098c1ecfe1787c52eb38e258f4fe4e24942136
Maftun Hashimli <maftunhashimli@gmail.com>
2023-03-20 05:33:43 +0300

    add thread example

---
+++ src/main/java/examples/thread/Application.java
@@ -0,0 +1,10 @@
+package examples.thread;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class Application {
+    public static void main(String[] args) {
+        Runner runner = new Runner();
+
+        runner.start();
+    }
+}
