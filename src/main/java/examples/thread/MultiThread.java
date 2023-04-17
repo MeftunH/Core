d116f08be05fe57e37b179ba740e3f9f4a394ef0
Maftun Hashimli <maftunhashimli@gmail.com>
2023-04-17 23:36:02 +0300

    add multi thread example

---
+++ src/main/java/examples/thread/MultiThread.java
@@ -0,0 +1,25 @@
+package examples.thread;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+public class MultiThread {
+    public static void main(String[] args) {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Hello: " + i);
+                }
+            }
+        });
+        t1.start();
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Hello: " + i);
+                }
+            }
+        });
+    }
+
+}
