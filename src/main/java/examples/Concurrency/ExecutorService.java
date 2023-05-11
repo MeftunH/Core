d4df69ad353c87f7ce2b6cfaad7b38652b7f9965
Maftun Hashimli <maftunhashimli@gmail.com>
2023-05-11 23:25:04 +0300

    add executor example

---
+++ src/main/java/examples/Concurrency/ExecutorService.java
@@ -0,0 +1,16 @@
+package examples.Concurrency;
+/* @author - Maftun Hashimli (maftunhashimli@gmail.com)) */
+
+import java.util.concurrent.Executors;
+
+public class ExecutorService {
+    public static void main(String[] args) {
+        java.util.concurrent.ExecutorService executorService = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(() -> {
+                // code to be executed concurrently
+            });
+        }
+        executorService.shutdown();
+    }
+}
