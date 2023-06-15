19cc80577626b713eb67417564009ec8024f02ae
Maftun Hashimli <maftunhashimli@gmail.com>
2023-06-15 23:37:51 +0300

    add URLify example

--- src/main/java/examples/Array/URLify.java
+++ src/main/java/examples/Array/URLify.java
@@ -5,9 +5,10 @@ public class URLify {
     public static void replaceSpaces(char[] str, int trueLength) {
         int spaceCount = 0, index, i = 0;
         for (i = 0; i < trueLength; i++) {
-            if (str[i] == ' ') {
-                spaceCount++;
-            }
+            if (str[i] != ' ')
+                continue;
+
+            spaceCount++;
         }
         index = trueLength + spaceCount * 2;
         if (trueLength < str.length) str[trueLength] = '\0';
@@ -17,10 +18,11 @@ public class URLify {
                 str[index - 2] = '2';
                 str[index - 3] = '%';
                 index = index - 3;
-            } else {
-                str[index - 1] = str[i];
-                index--;
+                continue;
             }
+
+            str[index - 1] = str[i];
+            index--;
         }
     }
 
